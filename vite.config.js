ac46ea973cc023e160ff1073eadd7321e65242a8
John Renner <j@cubist.dev>
2024-06-20 17:46:57 -0400

    build stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
+import template from "rollup-plugin-html-literals";
+import analyze from "rollup-plugin-analyzer";
+import fs from "node:fs";
 import { defineConfig } from "vite";
 
 const base = process.env.BASE_URL ?? "/";
 
 export default defineConfig({
   base,
+  build: {
+    target: "esnext",
+    modulePreload: false,
+  },
+  plugins: [
+    template(),
+    analyze({
+      writeTo: (stats) => {
+        fs.writeFileSync("dist/stats.html", stats);
+      },
+    }),
+  ],
 });
